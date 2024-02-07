333fe4367727feb9e9baa520c85bac7275e31a2c
Muhammad Bayu Ariyadi <bayzuariya@gmail.com>
2024-02-07 15:48:50 +0800

    menerapkan useEffect, ganti logo, dan nambah fitur halal

--- src/components/rickroll.jsx
+++ src/components/rickroll.jsx
@@ -1,13 +1,13 @@
 import ReactPlayer from "react-player";
 
-const Rickroll = () => {
+const Rickroll = (props) => {
     const embedUrl = 'https://www.youtube.com/embed/dQw4w9WgXcQ?si=WrCb_a3ft2JXow56&amp;controls=0&autoplay=1'
     return (
         
             <div className="player-wrapper" >
                 <ReactPlayer
                     className="react-player"
-                    url={embedUrl}
+                    url={props.url}
                     playing={true}
                     width="560px"
                     height="315px"
